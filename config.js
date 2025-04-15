94b1e40cf236f67894a6620b11d221d29d39f11a
Vibhav Darsha <66336210+vdarsha@users.noreply.github.com>
2025-04-15 13:29:38 -0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDvQvYVbzsfKk_-TTlIruZt7Tf28b_XG_4",
+  authDomain: "deskersizer.firebaseapp.com",
+  projectId: "deskersizer",
+  storageBucket: "deskersizer.firebasestorage.app",
+  messagingSenderId: "848878159272",
+  appId: "1:848878159272:web:8626b5d20738370924c99d"
+};
