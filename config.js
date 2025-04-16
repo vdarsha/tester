0f39a7517300500c0ebd260d44aac795f9d0137c
Vibhav Darsha <66336210+vdarsha@users.noreply.github.com>
2025-04-15 20:51:34 -0700

    Delete config.js

--- config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDvQvYVbzsfKk_-TTlIruZt7Tf28b_XG_4",
-  authDomain: "deskersizer.firebaseapp.com",
-  projectId: "deskersizer",
-  storageBucket: "deskersizer.firebasestorage.app",
-  messagingSenderId: "848878159272",
-  appId: "1:848878159272:web:8626b5d20738370924c99d"
-};
